c0e17e08e89ee3915c5d605dc7eaccd6863ee192
Mikit Nikitenok <mikita.nikitsionak@siliconmint.com>
2016-01-21 02:32:42 +0300

    add chart

---
+++ public/cahrt.js
@@ -0,0 +1,30 @@
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        XHR.get("/table.json").then(function(result) {
+            var table = [['create','sell','buy','nbrb']];
+            for(var i in result){
+              var tmp = [];
+              console.log(tmp[0]);
+              tmp.push(new Date(result[i].create));
+              tmp.push(+result[i].sell.replace(' ', ''));
+              tmp.push(+result[i].buy.replace(' ', ''));
+              tmp.push(+result[i].nbrb.replace(' ', ''));
+              table.push(tmp);
+            }
+
+        var data = google.visualization.arrayToDataTable(table);
+
+        var options = {
+          title: 'Company Performance',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+
+        });
+      }
